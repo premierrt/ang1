0d977fca4b1ba180d57b16cabfeeb9cf2c8f5a39
rt <>
2017-06-25 00:18:39 +0200

    Rest service

--- src/app/restapi/restservice.service.ts
+++ src/app/restapi/restservice.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {environment} from 'environments/environment';
 import {Http, Response} from '@angular/http';
 import {Todo} from '../todo/todo';
+import { Observable } from 'rxjs/Observable';
+
 
 const API_URL = environment.apiUrl;
 
@@ -10,6 +12,31 @@ export class RestserviceService {
 
   constructor(private http: Http) {
        
+  }
+
+// API: GET /todos
+  public getAllTodos() {
+    // will use this.http.get()
+  }
+
+  // API: POST /todos
+  public createTodo(todo: Todo) {
+    // will use this.http.post()
+  }
+
+  // API: GET /todos/:id
+  public getTodoById(todoId: number) {
+    // will use this.http.get()
+  }
+
+  // API: PUT /todos/:id
+  public updateTodo(todo: Todo) {
+    // will use this.http.put()
+  }
+
+  // DELETE /todos/:id
+  public deleteTodoById(todoId: number) {
+    // will use this.http.delete()
   }
 
 }
